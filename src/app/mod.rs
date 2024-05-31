e0065d38a98aac0b6359c57fed9be938f37d18db
Tzian <1164336+Tzian@users.noreply.github.com>
2024-05-31 05:01:56 +0000

    feat(proj): added pages mod

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -4,28 +4,29 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
-use crate::app::pages::front::FrontPage;
-use crate::app::pages::NotFound;
+use crate::app::pages::{home::HomePage, test::TestPage, NotFound};
 
 #[component]
-pub fn App() -> impl IntoView {
-    // Provides context that manages stylesheets, titles, meta tags, etc.
-    provide_meta_context();
+pub fn App() -> impl IntoView
+{
+	// Provides context that manages stylesheets, titles, meta tags, etc.
+	provide_meta_context();
 
-    view! {
-        <Stylesheet id="leptos" href="/pkg/leptos-dev.css"/>
+	view! {
+		<Stylesheet id="leptos" href="/pkg/leptos-dev.css"/>
 
-        // sets the document title
-        <Title text="Welcome to Leptos"/>
+		// sets the document title
+		<Title text="Welcome to Leptos"/>
 
-        // content for this welcome page
-        <Router>
-            <main style="background-color: lightblue; padding: 10px">
-                <Routes>
-                    <Route path="" view=FrontPage/>
-                    <Route path="/*any" view=NotFound/>
-                </Routes>
-            </main>
-        </Router>
-    }
+		// content for this welcome page
+		<Router>
+			<main style="background-color: lightblue; padding: 10px">
+				<Routes>
+					<Route path="" view=HomePage/>
+					<Route path="/test" view=TestPage/>
+					<Route path="/*any" view=NotFound/>
+				</Routes>
+			</main>
+		</Router>
+	}
 }
---
+++ src/app/pages/home.rs
@@ -0,0 +1,40 @@
+use leptos::*;
+use leptos_meta::Title;
+
+#[component]
+pub(crate) fn HomePage() -> impl IntoView
+{
+	view! {
+		<div class="bg-primary-900 text-white">
+			<div class="">
+				<a href="/" class="font-bold text-xl text-left ml-10 ">
+					"LeptosDev"
+				</a>
+
+			</div>
+		</div>
+
+		<Title text="LeptosDev"/>
+
+		<div class="text-center margin-top-8">
+			<p class="h0">"Leptos Dev!"</p>
+			<h2 class="txt-primary margin-top-5">"Some text about the app"</h2>
+
+			<h4 class="txt-tertiary margin-top-3">"More text about the app"</h4>
+
+			<div class="margin-top-5">
+				<a href="/login" class="std-btn">
+					"Login"
+				</a>
+			</div>
+
+			<h6 class="txt-tertiary margin-top-3">"or"</h6>
+
+			<div class="margin-top-3">
+				<a href="/register" class="std-btn" type="submit">
+					"Register"
+				</a>
+			</div>
+		</div>
+	}
+}
