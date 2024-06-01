ecd344e3b6aa5661763002e99909f7757549cc75
Tzian <1164336+Tzian@users.noreply.github.com>
2024-06-01 07:53:00 +0000

    feat(service): added ls_service crate

---
+++ ls_service/src/utils/jwt.rs
@@ -0,0 +1,42 @@
+use chrono::{Duration, Utc};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, TokenData, Validation};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims
+{
+	pub exp:   usize,
+	pub iat:   usize,
+	pub email: String,
+	pub id:    i32
+}
+
+pub fn encode_jwt(email: String, id: i32) -> Result<String, jsonwebtoken::errors::Error>
+{
+	let now = Utc::now();
+	let expiry = Duration::minutes(10);
+
+	let claims = Claims { exp: (now + expiry).timestamp() as usize,
+	                      iat: now.timestamp() as usize,
+	                      id,
+	                      email };
+
+	dotenvy::dotenv().ok();
+
+	let secret = std::env::var("JWT_SECRET").expect("JWT_SECRET not found");
+
+	encode(&Header::default(), &claims, &EncodingKey::from_secret(secret.as_ref()))
+}
+
+pub fn decode_jwt(jwt: String) -> Result<TokenData<Claims>, jsonwebtoken::errors::Error>
+{
+	println!("Decoding JWT");
+	println!("JWT is {}", jwt);
+	dotenvy::dotenv().ok();
+
+	let secret = std::env::var("JWT_SECRET").unwrap();
+	// println!("Secret is {}", secret);
+	let claim_data: Result<TokenData<Claims>, jsonwebtoken::errors::Error> = decode(&jwt, &DecodingKey::from_secret(secret.as_ref()), &Validation::default());
+
+	claim_data
+}
---
+++ ls_service/src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod api_response;
+pub mod auth_middlewares;
+pub mod jwt;
