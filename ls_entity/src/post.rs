35788ef5643aa6c1c3b83b34a593351a90b3671a
Tzian <1164336+Tzian@users.noreply.github.com>
2024-06-01 06:53:08 +0000

    feat(entity): added ls_entity crate

---
+++ ls_entity/src/post.rs
@@ -0,0 +1,43 @@
+use sea_orm::{entity::prelude::*, FromJsonQueryResult};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone,
+           Debug,
+           PartialEq,
+           Eq,
+           DeriveEntityModel,
+           Deserialize,
+           Serialize,
+           FromJsonQueryResult)]
+#[sea_orm(table_name = "posts")]
+pub struct Model
+{
+    #[sea_orm(primary_key)]
+    #[serde(skip_deserializing)]
+    pub id:      i32,
+    pub user_id: i32,
+    pub title:   String,
+    #[sea_orm(column_type = "Text")]
+    pub text:    String
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation
+{
+    #[sea_orm(belongs_to = "super::user::Entity",
+              from = "Column::UserId",
+              to = "super::user::Column::Id")]
+    User
+}
+
+impl Related<super::user::Entity> for Entity
+{
+    fn to() -> RelationDef
+    {
+        Relation::User.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel
+{
+}
