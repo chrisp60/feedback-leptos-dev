b2274166d55e2b6ba69c1f7aa78ec92a931337a6
Chris <89366859+chrisp60@users.noreply.github.com>
2024-07-04 16:17:18 -0400

    login: use nested routing
    This greatly simplifies the routing logic into a single condition check.
    All nested routes are conditionally displayed if the user is logged in,
    otherwise showing the `LoginPage` if not

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -11,34 +11,40 @@ pub mod sidebar;
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+use leptos_use::{use_cookie, utils::FromToStringCodec};
+
+use crate::{
+    app::{
+        dark_mode::DarkModeToggle,
+        navbar::NavBar,
+        pages::{
+            dash::dashboard::DashboardPage,
+            home::HomePage,
+            user::{login::LoginPage, register::RegisterPage},
+            NotFound,
+        },
+    },
+    server_fns::user::current::get_current_user,
+};
 
 #[component]
 pub fn App() -> impl IntoView {
-    use leptos_use::{use_cookie, utils::FromToStringCodec};
-
-    use crate::{
-        app::{
-            dark_mode::DarkModeToggle,
-            navbar::NavBar,
-            pages::{
-                dash::dashboard::DashboardPage,
-                home::HomePage,
-                user::{login::LoginPage, register::RegisterPage},
-                NotFound,
-            },
-        },
-        server_fns::user::current::get_current_user,
-    };
-
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
 
     // Determine if there is a logged in user cookie
-    let (access_token, _) = use_cookie::<String, FromToStringCodec>("leptos_access_token");
+    let (token, _) = use_cookie::<String, FromToStringCodec>("leptos_access_token");
 
     // Needs to be a blocking resource because we need to wait for the cookie result
     // before use in view
-    let usr = create_blocking_resource(move || access_token.get(), get_current_user);
+    let user = create_blocking_resource(token, get_current_user);
+
+    let logged_in = move || {
+        user.and_then(Option::is_some)
+            .transpose()
+            .unwrap_or_default()
+            .unwrap_or_default()
+    };
 
     view! {
         <Stylesheet id="leptos" href="/pkg/leptos-dev.css"/>
@@ -55,123 +61,29 @@ pub fn App() -> impl IntoView {
             </div>
         </div>
 
-        // content for this welcome page
-        <Router>
-            <main>
+        <main>
+            <Router fallback=move || view! { <NotFound/> }>
                 <Routes>
                     <Route
                         path="/"
                         view=move || {
                             view! {
-                                <Suspense fallback=move || {
-                                    "Loading...."
-                                }>
-                                    {move || {
-                                        usr.get()
-                                            .map(|data| {
-                                                let rr = data.unwrap();
-                                                view! {
-                                                    <Show
-                                                        when=move || rr.is_none()
-                                                        fallback=move || view! { <Redirect path="/dashboard"/> }
-                                                    >
-                                                        <HomePage/>
-                                                    </Show>
-                                                }
-                                            })
-                                    }}
-
+                                <Suspense>
+                                    <Show when=logged_in fallback=LoginPage>
+                                        <Outlet/>
+                                    </Show>
                                 </Suspense>
                             }
                         }
-                    />
+                    >
 
-                    <Route
-                        path="/register"
-                        view=move || {
-                            view! {
-                                <Suspense fallback=move || {
-                                    "Loading...."
-                                }>
-                                    {move || {
-                                        usr.get()
-                                            .map(|data| {
-                                                let rr = data.unwrap();
-                                                view! {
-                                                    <Show
-                                                        when=move || rr.is_none()
-                                                        fallback=move || view! { <Redirect path="/dashboard"/> }
-                                                    >
-                                                        <RegisterPage/>
-                                                    </Show>
-                                                }
-                                            })
-                                    }}
-
-                                </Suspense>
-                            }
-                        }
-                    />
-
-                    <Route
-                        path="/login"
-                        view=move || {
-                            view! {
-                                <Suspense fallback=move || {
-                                    "Loading...."
-                                }>
-                                    {move || {
-                                        usr.get()
-                                            .map(|data| {
-                                                let rr = data.unwrap();
-                                                view! {
-                                                    <Show
-                                                        when=move || rr.is_none()
-                                                        fallback=move || view! { <Redirect path="/dashboard"/> }
-                                                    >
-                                                        <LoginPage/>
-                                                    </Show>
-                                                }
-                                            })
-                                    }}
-
-                                </Suspense>
-                            }
-                        }
-                    />
-
-                    <Route
-                        path="/dashboard"
-                        view=move || {
-                            view! {
-                                <Suspense fallback=move || {
-                                    "Loading...."
-                                }>
-                                    {move || {
-                                        usr.get()
-                                            .map(|data| {
-                                                let rr = data.unwrap();
-                                                view! {
-                                                    <Show
-                                                        when=move || rr.is_some()
-                                                        fallback=move || view! { <Redirect path="/login"/> }
-                                                    >
-
-                                                        <DashboardPage/>
-
-                                                    </Show>
-                                                }
-                                            })
-                                    }}
-
-                                </Suspense>
-                            }
-                        }
-                    />
-
-                    <Route path="/*any" view=NotFound/>
+                        <Route path="dashboard" view=DashboardPage/>
+                        <Route path="register" view=RegisterPage/>
+                        <Route path="login" view=LoginPage/>
+                        <Route path="" view=HomePage/>
+                    </Route>
                 </Routes>
-            </main>
-        </Router>
+            </Router>
+        </main>
     }
 }
